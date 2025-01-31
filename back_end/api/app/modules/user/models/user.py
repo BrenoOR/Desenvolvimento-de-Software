ab1836b6ae9109c4eb3d90dc1913c8cd68b46a9c
andrelzl <andreluizcar399@gmail.com>
2025-01-31 00:07:51 -0300

    initializing crud

--- back_end/api/app/modules/user/models/user.py
+++ back_end/api/app/modules/user/models/user.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from typing import List
 from pydantic import Field
-
+from loguru import logger
 from modules.utils import BaseModel
 from modules.utils import generate_id
 
@@ -11,11 +11,10 @@ from modules.utils import generate_id
 class User(BaseModel):
     """User model."""
 
-    id: str = Field(
+    user_id: str = Field(
         None,
         title="User ID",
         description="Unique user ID.",
-        default_factory=generate_id,
     )
     username: str = Field(
         None, title="Username", description="User name.", max_length=50
@@ -24,14 +23,14 @@ class User(BaseModel):
         None,
         title="Email",
         description="User email.",
-        regex=r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+",
+#        pattern="[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+",
     )
     password: str = Field(
         None,
         title="Password",
         description="User password.",
         min_length=8,
-        regex=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])\S{8,}$",
+#        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])\S{8,}$",
     )
     hyperfocuses: List[str] = Field(
         None, title="Hyperfocuses", description="User hyperfocuses IDs."
@@ -49,7 +48,34 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        logger.info(f"Creating User with data: {self.dict()}")
+        self.user_id = generate_id()
+        logger.info(f"User ID: {self.user_id}")
         self.is_active = self.is_active if self.is_active is not None else True
         self.is_superuser = (
             self.is_superuser if self.is_superuser is not None else False
         )
+
+class UserResponse(BaseModel):
+    user_id: str = Field(
+        None,
+        title="User ID",
+        description="Unique user ID.",
+    )
+    username: str = Field(
+        None, title="Username", description="User name.", max_length=50
+    )
+    email: str = Field(
+        None,
+        title="Email",
+        description="User email.",
+#        pattern="[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+",
+    )
+    hyperfocuses: List[str] = Field(
+        None, title="Hyperfocuses", description="User hyperfocuses IDs."
+    )
+    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
