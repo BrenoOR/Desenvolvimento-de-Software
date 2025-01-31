b2a5b53699eee316372e1214c65cd8bb63930af3
ennalyc <ecsl2@cin.ufpe.br>
2025-01-31 14:50:13 -0300

    fixed some route errors

--- front_end/app/(root)/(auth)/(register)/pronouns.tsx
+++ front_end/app/(root)/(auth)/(register)/pronouns.tsx
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react'
-import { Link } from 'expo-router'
+import Link from 'expo-router/link'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { LinearGradient } from 'expo-linear-gradient';
 import { TouchableOpacity } from 'react-native';
@@ -29,19 +29,19 @@ const Pronouns = () => {
     {date: ''}
   )
   return (
-    <SafeAreaView className='h-full'>
-      <ScrollView contentContainerClassName='h-full'>
+    <SafeAreaView className='flex-1'>
+      <ScrollView contentContainerClassName='flex-grow'>
         <LinearGradient colors={["#ffffff", "#fbc7a0", "#fda0ec", "#a36ce6", "#39c0fb", "#201c1b"]}  locations={[0.65, 0.68, 0.71, 0.74, 0.77, 0.80]}>
           <View className='h-full w-full justify-start items-center p-8'>
              <Image source={icons.logo} className='h-8 w-40 mb-8'/>
-             <Text className='text-7xl font-bold mb-4'>Quais seus pronomes?</Text>
+             <Text className='text-6xl font-bold mb-4 self-start'>Quais seus pronomes?</Text>
              <View className='h-24'>
              <ScrollView horizontal={true} scrollEnabled={true}>
                 <View className='flex-row items-baseline w-screen'>
                   
                 {pronouns.map((myPronoun, index) => (
                   
-                  <TouchableOpacity className='m-2 border border-gray-300 rounded-3xl p-3'
+                  <TouchableOpacity className='m-2 border border-gray-300 rounded-3xl p-2'
                     key={index}
                     onPress={() => togglepronoun(myPronoun)}
                     style={{
@@ -49,24 +49,24 @@ const Pronouns = () => {
                     }}
                     
                   >
-                    <Text className='text-2xl'>{myPronoun}</Text>
+                    <Text className='text-xl'>{myPronoun}</Text>
               </TouchableOpacity>
               ))}
               </View>
               </ScrollView>
              </View>
-             <View className='w-screen mx-8 p-8'>
-             <Text className='text-4xl font-bold mb-4 self-start p-2'>Data de nascimento</Text>
+             <View className='w-screen mx-8 px-8 mb-32'>
+             <Text className='text-3xl font-bold mb-4 self-start'>Data de nascimento</Text>
               <FormField
               title={"DD/MM/YYYY"}
               value={form.date}
               handleChangeText={(e: any) => setForm({...form, date: e})}
               keyboardType="date"
-              otherStyle="bg-gray200 opacity-[.75] rounded-2xl border border-gray-700"
+              otherStyle="bg-gray200 opacity-[.75] rounded-2xl border border-gray-700 h-16 justify-center items-center"
               />
              </View>
-              <TouchableOpacity className="w-3/4 bg-primary rounded-full h-24 justify-center items-center mt-48">
-                <Link href={"/home"}>
+              <TouchableOpacity className="w-3/4 bg-primary rounded-full h-24 justify-center items-center">
+                <Link href={"/new-connection"}>
                   <Text className="text-3xl font-bold text-black">
                     Concluir
                   </Text>
